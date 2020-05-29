4b855f6fc6aaaef63f14fe7a3c0f8dbeb430179f
unknown <mariaservicetest@gmail.com>
2020-05-28 23:11:10 -0500

    conexion a mysql 8.0.0

--- backenninja/backenninja/src/main/java/com/undemy/BackenninjaApplication.java
+++ backenninja/backenninja/src/main/java/com/undemy/BackenninjaApplication.java
@@ -2,13 +2,38 @@ package com.undemy;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class BackenninjaApplication {
+public class BackenninjaApplication implements CommandLineRunner {
+	@Autowired
+    private BookRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackenninjaApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		  repository.save(new Book("Dart"));
+	        repository.save(new Book("Angular"));
+	        repository.save(new Book("Linux"));
+
+	        System.out.println("\nfindAll()");
+	        repository.findAll().forEach(x -> System.out.println(x));
+
+	        System.out.println("\nfindById(1L)");
+	        repository.findById(1l).ifPresent(x -> System.out.println(x));
+
+	        System.out.println("\nfindByName('Linux')");
+	        repository.findByName("Linux").forEach(x -> System.out.println(x));
+	}
+	
+	
 }
---
+++ backenninja/backenninja/src/main/java/com/undemy/BookRepository.java
@@ -0,0 +1,13 @@
+package com.undemy;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByName(String name);
+
+}
